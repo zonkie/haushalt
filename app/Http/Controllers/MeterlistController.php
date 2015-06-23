07a9158a3be53049bc135e6e0604d34725cf9f4e
Soenke Domroese <soenke@soenke-domroese.de>
2015-06-23 16:38:44 +0200

    Revert "huargh"
    This reverts commit 45a3ae2581a94545b042ba4c230c8bb458b4c8f6.

--- app/Http/Controllers/MeterlistController.php
+++ app/Http/Controllers/MeterlistController.php
@@ -1,94 +1,74 @@
 <?php
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use DB;
-use App\Http\Controllers\Controller;
+    use DB;
+    use App\Http\Controllers\Controller;
 
-class MeterlistController
-    extends Controller {
+    class MeterlistController extends Controller
+    {
 
-    /*
-    |--------------------------------------------------------------------------
-    | Tutorial Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller renders your application's "dashboard" for users that
-    | are authenticated. Of course, you are free to change or remove the
-    | controller as you wish. It is just here to get your app started!
-    |
-    */
+        /*
+        |--------------------------------------------------------------------------
+        | Tutorial Controller
+        |--------------------------------------------------------------------------
+        |
+        | This controller renders your application's "dashboard" for users that
+        | are authenticated. Of course, you are free to change or remove the
+        | controller as you wish. It is just here to get your app started!
+        |
+        */
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct() {
-        //$this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard to the user.
-     *
-     * @return Response
-     */
-    public function index() {
-        $locations = DB::select('select * from locations where active = ?', [ 1 ]);
-        foreach ($locations AS &$location) {
-            $location->meters = DB::select('select * from meters where location = ? AND active = ?', [ $location->id, 1 ]);
+        /**
+         * Create a new controller instance.
+         *
+         * @return void
+         */
+        public function __construct()
+        {
+            //$this->middleware('auth');
         }
 
-        return view('meters.list', [ 'locations' => $locations ]);
-    }
+        /**
+         * Show the application dashboard to the user.
+         *
+         * @return Response
+         */
+        public function index()
+        {   $locations = DB::select('select * from locations where active = ?', [1]);
+            foreach($locations AS &$location){
 
-    public function meter($meter_id) {
-        $meter = DB::select('select * from meters where location = ?' , [ $meter_id ]);
-        $values = DB::select('select * from meter_values where meter = ?', [ $meter_id ]);
-
-        $beforeValue  = null;
-        $begin_date   = null;
-        $end_date     = null;
-        $days_total   = 0;
-        $aGraphValues = array(
-            'total'         => array(),
-            'diff'          => array(),
-            'diffPerPerson' => array(),
-        );
-        foreach ($values AS &$value) {
-            if (is_null($begin_date)) {
-                $begin_date = new \DateTime($value->read_date);
+                $location->meters = DB::select('select * from meters where location = '. $location->id .' AND active = ?', [1]);
             }
-
-            $read_date  = new \DateTime($value->read_date);
-            $interval   = ( ! is_null($end_date) ? $end_date->diff($read_date)->format('%a') : 0 );
-            $days_total = $days_total + $interval;
-
-            $value->days = $days_total;
-
-            $end_date = new \DateTime($value->read_date);
-
-            if (! is_null($beforeValue)) {
-                $value->diff          = ( $value->value - $beforeValue );
-                $value->diffPerPerson = ( $value->diff / $value->persons );
-                $value->diffPerDay          = ($value->diff / $interval); //number_format(...,3,',','');
-                $value->diffPerPersonPerDay = ($value->diffPerDay / $interval); //number_format(...,3,',','');
-            } else {
-                $value->diff                = 0;
-                $value->diffPerPerson       = 0;
-                $value->diffPerDay          = 0;
-                $value->diffPerPersonPerDay = 0;
-            }
-
-            $beforeValue                                   = $value->value;
-            $aGraphValues['total'][ strtotime($read_date->format('Y-m-d')) ]         = $value->value;
-            $aGraphValues['diff'][ strtotime($read_date->format('Y-m-d')) ]          = $value->diff;
-            $aGraphValues['diffPerPerson'][ strtotime($read_date->format('Y-m-d')) ] = $value->diffPerPerson;
-            $aGraphValues['diffPerDay'][ strtotime($read_date->format('Y-m-d')) ]          = $value->diffPerDay;
-            $aGraphValues['diffPerPersonPerDay'][ strtotime($read_date->format('Y-m-d')) ] = $value->diffPerPersonPerDay;
+            return view('meters.list', ['locations' => $locations]);
         }
 
-        return view('meters.detail', [ 'meter'=> $meter, 'metervalues' => $values, 'startDate' => $begin_date, 'endDate' => $end_date, 'graphValues' => $aGraphValues ]);
+        public function meter($meter_id){
+
+            $values = DB::select('select * from meter_values where meter = ?', [$meter_id]);
+
+            $beforeValue = null ;
+            $begin_date = null;
+            $end_Date = null;
+
+            foreach($values AS &$value){
+                if(is_null($begin_date)){
+                    $begin_date = date("Y-m-d",strtotime($value->read_date));
+                }
+
+                $end_date = date("Y-m-d",strtotime($value->read_date));
+                $value->read_date = date("Y-m-d",strtotime($value->read_date));
+                if(!is_null($beforeValue)){
+                    $value->diff = ($value->value - $beforeValue);
+                    $value->diffPerPerson = ($value->diff / $value->persons);
+                } else {
+                    $value->diff = 0;
+                    $value->diffPerPerson = 0;
+                }
+                $beforeValue = $value->value;
+            }
+
+            return view('meters.detail', ['metervalues' => $values, 'startDate' => $begin_date, 'endDate' =>$end_Date]);
+            
+        }
 
     }
-
-}
--- public/js/meter.js
+++ public/js/meter.js
@@ -3,72 +3,38 @@ jQuery(document).ready(function() {
         hoverable : true,
         shadowSize: 0,
         series    : {
-            lines : {show: true},
-            points: {show: true}
+            lines : { show: true },
+            points: { show: true }
         },
         xaxis     : {
-            mode              : "time",
-            tickSize          : [1, "day"],
-            axisLabelUseCanvas: true,
-            tickLength        : 0
+            tickLength: 0
         },
-        yaxes     : [
-            {
-                axisLabel: "Absolute",
-                axisLabelUseCanvas: true,
-                axisLabelFontSizePixels: 12,
-                axisLabelFontFamily: 'Verdana, Arial',
-                axisLabelPadding: 3
-            },
-            {
-                position: "right",
-                axisLabel: "Per Person",
-                axisLabelUseCanvas: true,
-                axisLabelFontSizePixels: 12,
-                axisLabelFontFamily: 'Verdana, Arial',
-                axisLabelPadding: 3
-            },
-            {
-                position: "right",
-                axisLabel: "Per Person Per Day",
-                axisLabelUseCanvas: true,
-                axisLabelFontSizePixels: 12,
-                axisLabelFontFamily: 'Verdana, Arial',
-                axisLabelPadding: 3
-            }
-        ],
+        yaxis     : {
+            //ticks       : 10,
+            //min         : 50,
+            //max         : 200,
+            //tickDecimals: 3
+        },
         grid      : {
-            backgroundColor: {colors: ["#fff", "#EEE"]},
+            backgroundColor: { colors: ["#fff", "#fff"] },
             borderWidth    : 1,
-            borderColor    : '#555',
-            hoverable: true
+            borderColor    : '#555'
         },
-        legend    : {
-            show           : true,
-            position       : "se",
-            backgroundColor: null,
-            opacity: 0.7
+        legend: {
+            show: true,
+            position: "se",
+            backgroundColor: null
         },
-        colors    : ['#FF9900', '#00FF00', '#AB0000', '#000099', '#009900']
+        colors: ['#FF9900', '#00FF00', '#AB0000', '#000099']
     };
     var meter_charts_absolute = jQuery('#meter-charts-absolute').css({'height': '220px'});
-    var meter_charts_diff = jQuery('#meter-charts-diff').css({'height': '220px'});
-    var meter_charts_day = jQuery('#meter-charts-day').css({'height': '220px'});
+    var meter_charts_absolute = jQuery('#meter-charts-diff').css({'height': '220px'});
 
     jQuery.plot("#meter-charts-absolute", [
-        {label: "Absolute", data: dataTotal},
-        //{label: "Total Usage", data: dataUsageTotal, yaxis: 2},
-        //{label: "Total Usage Per Person", data: dataUsagePerPerson, yaxis: 2},
-        //{label: "Daily usage", data: dataUsageTotalPerDay,yaxis: 3},
-        //{label: "Daily usage Per Person", data: dataUsagePerPersonPerDay,yaxis: 3}
-    ], options);
+        { label: "Absolute", data: dataTotal },
+    ],options );
     jQuery.plot("#meter-charts-diff", [
-        {label: "Total Usage", data: dataUsageTotal},
-        {label: "Total Usage Per Person", data: dataUsagePerPerson}
-    ], options);
-
-    jQuery.plot("#meter-charts-day", [
-        {label: "Daily usage", data: dataUsageTotalPerDay},
-        {label: "Daily usage Per Person", data: dataUsagePerPersonPerDay}
-    ], options);
+        { label: "Usage Total", data: dataUsageTotal },
+        { label: "Usage Per Person", data: dataUsagePerPerson }
+    ],options );
 });
